73d50a001e8d95b7e1811e6a35d51ada837ba7dc
Aditya Raj <140353273+AdityaRaj261@users.noreply.github.com>
2024-01-27 00:10:21 +0530

    Add files via upload

---
+++ Assigement(pointers).cpp/intp,q.cpp
@@ -0,0 +1,12 @@
+// int *p , q;
+#include<iostream>
+using namespace std;
+int main(){
+      int *p,q;
+      int *p = &q;
+}                    // p is a pointer and q is interger
+
+
+
+
+
---
+++ Assigement(pointers).cpp/snippnetcorrectOrNot.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int a = 10, b =20;
+    int *ptr;
+    *ptr = 5;  //correct
+}
